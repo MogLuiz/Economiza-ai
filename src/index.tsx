4f9a7a293d19356e29d07db764a0f6a5b9848950
Luiz <luiz>
2021-03-27 20:15:00 -0300

    Commit inicial do meu projeto

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Dashboard from './pages/Dashboard';
+
+import App from './pages/Dashboard';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Dashboard />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
