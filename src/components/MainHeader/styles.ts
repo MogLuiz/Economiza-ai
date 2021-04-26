1979903257b250eb1a6ab1974785b689709b8e7d
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-04-26 01:28:53 -0300

    Add files via upload

--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     grid-area: MH;
-    background-color:#252A48;
+    background-color: ${props => props.theme.colors.secondary};
+    color: ${props => props.theme.colors.white};
 
 `
