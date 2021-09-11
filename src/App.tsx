b104543fb7c70015d4da93499954fbaad74efc95
Luiz Henrique <luizhjramos@outlook.com>
2021-09-11 11:36:52 -0300

    feat: header list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import GlobalStyles from './styles/GlobalStyles';
-import {ThemeProvider} from 'styled-components'
+import React from "react";
+import GlobalStyles from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
 
-import Dashboard from './pages/Dashboard';
+import Dashboard from "./pages/Dashboard";
+import List from "./pages/List";
 
-import Layout from './components/Layout';
-import dark from './styles/themes/dark';
+import Layout from "./components/Layout";
+import dark from "./styles/themes/dark";
 
 const App: React.FC = () => {
-    return (
-        
-           <ThemeProvider theme={dark} >
-                <GlobalStyles />
-                <Layout>
-                   <Dashboard /> 
-                </Layout>
-           </ThemeProvider>
-        
-    )
-}
+  return (
+    <ThemeProvider theme={dark}>
+      <GlobalStyles />
+      <Layout>
+        <List />
+      </Layout>
+    </ThemeProvider>
+  );
+};
 
 export default App;
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import ContentHeader from "../../components/ContentHeader";
+import SelectInput from "../../components/SelectInput";
+import { Container } from "./style";
 
 const List: React.FC = () => {
-    return (
-        <h1>List</h1>
-    )
-}
+  const options = [
+    { value: "Luiz", label: "Luiz" },
+    { value: "Bruno", label: "Bruno" },
+    { value: "Lucas", label: "Lucas" },
+  ];
+  return (
+    <Container>
+      <ContentHeader title="Saidas" lineColor="#E44C4E">
+        <SelectInput options={options} />
+      </ContentHeader>
+    </Container>
+  );
+};
 
-export default List
+export default List;
