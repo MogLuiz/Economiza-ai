8b64558d61b0c8065f152ce8c1997036adc5530d
Luiz Henrique <luizhjramos@outlook.com>
2021-09-11 12:00:35 -0300

    feat: HistoryFinanceCard Skeleton

---
+++ src/components/HistoryFinanceCard/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const HistoryFinanceCard: React.FC = () => {
+  return <Container>a</Container>;
+};
+
+export default HistoryFinanceCard;
