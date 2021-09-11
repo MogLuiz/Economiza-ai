3c4dd09f1c2fd27e5fa28fae7f1f5dc1be453440
Luiz Henrique <luizhjramos@outlook.com>
2021-09-11 12:10:10 -0300

    feat: typing IHistoryFinance Card

--- src/components/HistoryFinanceCard/index.tsx
+++ src/components/HistoryFinanceCard/index.tsx
@@ -2,8 +2,22 @@ import React from "react";
 
 import { Container } from "./styles";
 
-const HistoryFinanceCard: React.FC = () => {
-  return <Container>a</Container>;
+interface IHistoryFinanceCardProps {
+  cardColor: string;
+  tagColor: string;
+  title: string;
+  subtitle: string;
+  amount: string;
+}
+
+const HistoryFinanceCard: React.FC<IHistoryFinanceCardProps> = ({
+  cardColor,
+  tagColor,
+  title,
+  subtitle,
+  amount,
+}) => {
+  return <Container></Container>;
 };
 
 export default HistoryFinanceCard;
