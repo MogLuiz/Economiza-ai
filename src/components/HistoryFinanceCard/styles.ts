b3b4a1c740bf8ed92c7a71d972b54d90f93619ae
Luiz Henrique <luizhjramos@outlook.com>
2021-09-11 14:57:17 -0300

    style HistoryFinanceCard component

--- src/components/HistoryFinanceCard/styles.ts
+++ src/components/HistoryFinanceCard/styles.ts
@@ -1,5 +1,56 @@
 import styled from "styled-components";
 
-export const Container = styled.div ``
+interface IContainerProps {
+    color: string;
+}
 
-export const Tag = styled.div ``
+interface ITagProps {
+    color: string;
+}
+
+export const Container = styled.li<IContainerProps> `
+    background-color: ${props => props.color};
+
+    list-style: none;
+    border-radius: 5px;
+
+    margin: 10px 0;
+    padding: 12px 10px;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    cursor: pointer;
+    transition: all .3s;
+
+    position: relative;
+
+    &:hover {
+        opacity: .7;
+        transform: translateX(10px);
+    }
+
+    > div {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+      
+        padding-left: 10px;
+    }
+
+    > div span {
+        margin-bottom: 2px;
+        font-weight: bold;
+    }
+`
+
+export const Tag = styled.div<ITagProps> `
+    width: 10px;
+    height: 60%;
+
+    background-color: ${props => props.color};
+
+    position: absolute;
+    left: 0;
+`
