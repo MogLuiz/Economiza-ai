143d99714a8c645600cdd5b05045515ee2328437
Luiz Henrique <luizhjramos@outlook.com>
2021-09-19 16:33:01 -0300

    feat: formatCurrency function

---
+++ src/utils/formatCurrency.ts
@@ -0,0 +1,11 @@
+const formatCurrency = (current: number): string => {
+    return current.toLocaleString(
+        'pt-br',
+        {
+            style: 'current',
+            currency: 'BRL'
+        }
+    )
+}
+
+export default formatCurrency
