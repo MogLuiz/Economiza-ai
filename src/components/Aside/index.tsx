c427bf29139daaa6de7290696d1d38cbc6bccc1f
Luiz Henrique <luizhjramos@outlook.com>
2021-09-18 22:24:10 -0300

    feat: path components

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,57 +1,54 @@
-import React from 'react'
+import React from "react";
 
-import logoImg from '../../assets/logo.svg'
-
-import {
-    MdDashboard,
-    MdArrowDownward,
-    MdArrowUpward,
-    MdExitToApp
-} from 'react-icons/md'
+import logoImg from "../../assets/logo.svg";
 
 import {
-        Container,
-        Header,
-        LogImg,
-        Title,
-        MenuContainer,
-        MenuItemLink
-} from './styles'
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp,
+} from "react-icons/md";
+
+import {
+  Container,
+  Header,
+  LogImg,
+  Title,
+  MenuContainer,
+  MenuItemLink,
+} from "./styles";
 
 const Aside: React.FC = () => {
-    return(
-        <Container>
-            <Header>
-                <LogImg src={logoImg} alt="Logo Economizaê" />
-                <Title>
-                    Economizaê
-                </Title>
-            </Header>
+  return (
+    <Container>
+      <Header>
+        <LogImg src={logoImg} alt="Logo Economizaê" />
+        <Title>Economizaê</Title>
+      </Header>
 
-            <MenuContainer>
-                <MenuItemLink href="#">
-                    <MdDashboard />
-                    Dashboard
-                </MenuItemLink>
+      <MenuContainer>
+        <MenuItemLink href="/dashboard">
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
 
-                <MenuItemLink href="#">
-                    <MdArrowUpward />
-                    Entradas
-                </MenuItemLink>
+        <MenuItemLink href="/list/entry-balance">
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
 
-                <MenuItemLink href="#">
-                    <MdArrowDownward />
-                    Saídas
-                </MenuItemLink>
+        <MenuItemLink href="/list/exit-balance">
+          <MdArrowDownward />
+          Saídas
+        </MenuItemLink>
 
-                <MenuItemLink href="#">
-                    <MdExitToApp />
-                    Sair
-                </MenuItemLink>
-            </MenuContainer>
-        </Container>
-    )
+        <MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+      </MenuContainer>
+    </Container>
+  );
+};
 
-}
-
-export default Aside
+export default Aside;
