8247344bc650587d7f9e271e4946b69ba7567d0a
Luiz Henrique <luizhjramos@outlook.com>
2021-09-19 16:44:29 -0300

    feat: formatDate function

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,14 @@
+const formatDate = (date: string): string => {
+
+    const dateFormatted = new Date(date)
+
+    const day = dateFormatted.getDay()
+
+    const month = dateFormatted.getMonth() + 1
+
+    const year = dateFormatted.getFullYear()
+
+    return `${day}/${month}/${year}`
+}
+
+export default formatDate
