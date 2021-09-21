4c5be1faadba2ef00d5c1f7914865d2db7806ade
Luiz Henrique <luizhjramos@outlook.com>
2021-09-21 20:30:36 -0300

    fix: color border bottom

--- src/pages/List/style.ts
+++ src/pages/List/style.ts
@@ -30,7 +30,7 @@ export const Filters = styled.div `
         display: block;
         width: 55px;
         margin: 0 auto;
-        border-bottom: 10px solid ${props => props.theme.colors.warning};  
+        border-bottom: 10px solid ${props => props.theme.colors.success};  
     }
 
     .eventual::after  {        
@@ -38,6 +38,6 @@ export const Filters = styled.div `
         display: block;
         width: 55px;
         margin: 0 auto;
-        border-bottom: 10px solid ${props => props.theme.colors.success};  
+        border-bottom: 10px solid ${props => props.theme.colors.warning};  
     }
 `
