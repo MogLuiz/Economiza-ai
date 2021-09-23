a6da6b437ba639c0565eab4c2e903e506ec26b2f
Luiz Henrique <luizhjramos@outlook.com>
2021-09-23 18:35:58 -0300

    feat: initialize WalletBox component

---
+++ src/components/WalletBox/index.tsx
@@ -0,0 +1,14 @@
+// Packages
+import React from "react";
+
+// Style
+import { Container } from "./styles";
+
+const WalletBox: React.FC = () => {
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
+  return <Container></Container>;
+};
+
+export default WalletBox;
