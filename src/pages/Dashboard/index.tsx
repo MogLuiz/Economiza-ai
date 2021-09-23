e23d295744031d61d6d80967538ee6f07f416a38
Luiz Henrique <luizhjramos@outlook.com>
2021-09-22 21:58:05 -0300

    feat: list of months and years

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,22 +1,85 @@
 // Packages
-import React from "react";
+import React, { useMemo, useState } from "react";
 
 // Components
 import SelectInput from "../../components/SelectInput";
 import ContentHeader from "../../components/ContentHeader";
+
+// Utils
+import listOfMonths from "../../utils/months";
+
+// Data
+import gains from "../../repositories/gains";
+import expenses from "../../repositories/expenses";
 
 // Style
 import { Container } from "./style";
 
 const Dashboard: React.FC = () => {
   // -------------------------------------------------
-  // Options
+  // States
   // -------------------------------------------------
-  const options = [
-    { value: "Luiz", label: "Luiz" },
-    { value: "Bruno", label: "Bruno" },
-    { value: "Lucas", label: "Lucas" },
-  ];
+  const [monthSelected, setMonthSelected] = useState<number>(
+    new Date().getMonth() + 1
+  );
+  const [yearSelected, setYearSelected] = useState<number>(
+    new Date().getFullYear()
+  );
+
+  // -------------------------------------------------
+  // Hooks
+  // -------------------------------------------------
+
+  const years = useMemo(() => {
+    let uniqueYears: number[] = [];
+
+    [...expenses, ...gains].forEach((item) => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+
+      if (!uniqueYears.includes(year)) {
+        uniqueYears.push(year);
+      }
+    });
+
+    return uniqueYears.map((year) => {
+      return {
+        value: year,
+        label: year,
+      };
+    });
+  }, []);
+
+  const months = useMemo(() => {
+    return listOfMonths.map((month, index) => {
+      return {
+        value: index + 1,
+        label: month,
+      };
+    });
+  }, []);
+
+  // -------------------------------------------------
+  // Functions
+  // -------------------------------------------------
+
+  const handleMonthSelected = (month: string) => {
+    try {
+      const parseMonth = Number(month);
+      setMonthSelected(parseMonth);
+    } catch (error) {
+      throw new Error("Invalid month value. Is accept - 0 - 24.");
+    }
+  };
+
+  const handleYearSelected = (year: string) => {
+    try {
+      const parseYear = Number(year);
+      setYearSelected(parseYear);
+    } catch (error) {
+      throw new Error("Invalid year value. Is accept - 0 - 12");
+    }
+  };
 
   // -------------------------------------------------
   // Render
@@ -24,7 +87,16 @@ const Dashboard: React.FC = () => {
   return (
     <Container>
       <ContentHeader title="Dashboard" lineColor="#F7931B">
-        <SelectInput options={options} onChange={() => {}} />
+        <SelectInput
+          options={months}
+          onChange={(e) => handleMonthSelected(e.target.value)}
+          defaultValue={monthSelected}
+        />
+        <SelectInput
+          options={years}
+          onChange={(e) => handleYearSelected(e.target.value)}
+          defaultValue={yearSelected}
+        />
       </ContentHeader>
     </Container>
   );
