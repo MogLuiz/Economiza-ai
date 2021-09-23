6f0a995c49285ec562eea0a4fdcf49eae87070a9
Luiz Henrique <luizhjramos@outlook.com>
2021-09-23 19:04:38 -0300

    feat: props component

--- src/components/WalletBox/index.tsx
+++ src/components/WalletBox/index.tsx
@@ -1,17 +1,35 @@
 // Packages
 import React from "react";
 
+// Assets
+import dollarImg from "../../assets/dollar.svg";
+import arrowUpImg from "../../assets/arrow-up.svg";
+import arrowDownImg from "../../assets/arrow-down.svg";
+
 // Types
 import { IWalletBoxProps } from "./types";
 
 // Style
 import { Container } from "./styles";
 
-const WalletBox: React.FC = () => {
+const WalletBox: React.FC<IWalletBoxProps> = ({
+  title,
+  amount,
+  footerlabel,
+  icon,
+  color,
+}) => {
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
-  return <Container></Container>;
+  return (
+    <Container color={color}>
+      <span>{title}</span>
+      <h1>{amount}</h1>
+      <small>{footerlabel}</small>
+      <img src={dollarImg} alt={title} />
+    </Container>
+  );
 };
 
 export default WalletBox;
