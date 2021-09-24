b26e8d7110b8cdd7d1876e8519c4bdbfac0f4759
Luiz Henrique <luizhjramos@outlook.com>
2021-09-24 12:58:12 -0300

    feat: header estructury component

--- src/components/MessageBox/index.tsx
+++ src/components/MessageBox/index.tsx
@@ -1,6 +1,9 @@
 // Packages
 import React from "react";
 
+// Assets
+import happyImg from "../../assets/happy.svg";
+
 // Style
 import { Container } from "./styles";
 
@@ -8,7 +11,16 @@ const MessageBox: React.FC = () => {
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
-  return <Container></Container>;
+  return (
+    <Container>
+      <header>
+        <h1>
+          Muito bem!
+          <img src={happyImg} alt="" />
+        </h1>
+      </header>
+    </Container>
+  );
 };
 
 export default MessageBox;
