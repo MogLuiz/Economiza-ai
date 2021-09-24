e87b9470a19cf74efc98e6888a3f2c66a5570bed
Luiz Henrique <luizhjramos@outlook.com>
2021-09-24 12:54:27 -0300

    feat: initialize MessageBox component

---
+++ src/components/MessageBox/index.tsx
@@ -0,0 +1,14 @@
+// Packages
+import React from "react";
+
+// Style
+import { Container } from "./styles";
+
+const MessageBox: React.FC = () => {
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
+  return <Container></Container>;
+};
+
+export default MessageBox;
