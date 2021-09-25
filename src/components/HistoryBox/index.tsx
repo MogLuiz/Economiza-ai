3a1e01d9c4e1400e875484ba11e1be52f968b535
Luiz Henrique <luizhjramos@outlook.com>
2021-09-25 17:13:53 -0300

    feat: HistoryBox component structure

---
+++ src/components/HistoryBox/index.tsx
@@ -0,0 +1,13 @@
+// Packages
+import React from "react";
+
+// Style
+import { Container } from "./styles";
+
+const HistoryBox: React.FC = () => (
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
+  <Container></Container>
+);
+export default HistoryBox;
