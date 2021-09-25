d9f2cc27710aa1c3b8cf8a62765684b069d9418e
Luiz Henrique <luizhjramos@outlook.com>
2021-09-24 22:07:53 -0300

    PieChat structure component

--- src/components/PieChart/index.tsx
+++ src/components/PieChart/index.tsx
@@ -1,14 +1,38 @@
 // Packages
 import React from "react";
+import { Pie, PieChart, Cell, ResponsiveContainer } from "recharts";
 
 // Style
 import { Container } from "./styles";
 
-const PieChart: React.FC = () => {
+const PieChart: React.FC = () => (
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
-  return <Container></Container>;
-};
+  <Container>
+    <SideLeft>
+      <LegendContainer>
+        <Legend>
+          <div>95%</div>
+          <span>Saídas</span>
+        </Legend>
+      </LegendContainer>
+    </SideLeft>
+
+    <SideRight>
+      <ResponsiveContainer>
+        <PieChart>
+          <Pie
+            data={[{ amount: 30, percent: 95 }]}
+            labelLine={false}
+            dataKey="percent"
+          >
+            {}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </SideRight>
+  </Container>
+);
 
 export default PieChart;
