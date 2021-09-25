0999a898c7f4269ea49538859af5d7a6ada48578
Luiz Henrique <luizhjramos@outlook.com>
2021-09-24 21:51:46 -0300

    feat: Initializing the PieChart Component Structure

---
+++ src/components/PieChart/index.tsx
@@ -0,0 +1,14 @@
+// Packages
+import React from "react";
+
+// Style
+import { Container } from "./styles";
+
+const PieChart: React.FC = () => {
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
+  return <Container></Container>;
+};
+
+export default PieChart;
