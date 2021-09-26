6817d0461c4151248a0f462826aab1078b5dc527
Luiz Henrique <luizhjramos@outlook.com>
2021-09-26 15:20:42 -0300

    feat: BarChartBox component initialization

---
+++ src/components/BarChartBox/index.tsx
@@ -0,0 +1,14 @@
+// Packages
+import React from "react";
+
+// Style
+import { Container } from "./styles";
+
+const BarChartBox: React.FC = () => {
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
+  return <Container></Container>;
+};
+
+export default BarChartBox;
