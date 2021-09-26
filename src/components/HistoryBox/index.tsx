74c06fb3924536dd90ea93d59cff8c18c3384e93
Luiz Henrique <luizhjramos@outlook.com>
2021-09-26 10:54:14 -0300

    feat: chart dimensions

--- src/components/HistoryBox/index.tsx
+++ src/components/HistoryBox/index.tsx
@@ -29,7 +29,10 @@ const HistoryBox: React.FC<IHistoryBoxProps> = ({
     <h2>Histórico de saldo</h2>
 
     <ResponsiveContainer>
-      <LineChart data={data}>
+      <LineChart
+        data={data}
+        margin={{ top: 5, right: 20, left: 20, bottom: 5 }}
+      >
         <CartesianGrid strokeDasharray="3 3" stroke="#cecece" />
         <XAxis dataKey="month" stroke="#cecece" />
         <Tooltip />
--- src/components/HistoryBox/styles.ts
+++ src/components/HistoryBox/styles.ts
@@ -1,3 +1,8 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const Container = styled.div`
+
+    width: 100%;
+    height: 340px;
+
+`
