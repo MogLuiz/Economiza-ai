e19fecf2ea53d3f7cb022d4c0ad60a7ac5f3de58
Luiz Henrique <luizhjramos@outlook.com>
2021-09-30 09:48:40 -0300

    feat: form structure

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,11 +1,32 @@
 // Packages
 import React from "react";
 
+// Assets
+import logoImg from "../../assets/logo.svg";
+
+import { Container, Logo, Form, FormTitle } from "./styles";
+
 const Signin: React.FC = () => {
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
-  return <h1>Signin</h1>;
+  return (
+    <Container>
+      <Logo>
+        <img src={logoImg} alt="Economizaê" />
+        <h3>Economizaê</h3>
+      </Logo>
+
+      <Form>
+        <FormTitle>Entrar</FormTitle>
+
+        <input type="text" />
+        <input type="text" />
+
+        <button type="submit">Acessar</button>
+      </Form>
+    </Container>
+  );
 };
 
 export default Signin;
