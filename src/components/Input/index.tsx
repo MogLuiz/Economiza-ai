79ea61a387a381095cd118d9846fafa1e7d32e61
Luiz Henrique <luizhjramos@outlook.com>
2021-09-30 18:16:01 -0300

    feat: Structure Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,14 @@
+// Packages
+import React from "react";
+
+// Style
+import { Container } from "./styles";
+
+const Input: React.FC = () => (
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
+  <Container></Container>
+);
+
+export default Input;
