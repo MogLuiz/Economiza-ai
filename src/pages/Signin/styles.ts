1b197ae2cebb79e130efe4d54736ade0aaead094
Luiz Henrique <luizhjramos@outlook.com>
2021-09-30 10:03:16 -0300

    feat: style container and logo

--- src/pages/Signin/styles.ts
+++ src/pages/Signin/styles.ts
@@ -1,3 +1,47 @@
 import styled from "styled-components";
 
-export const Container = styled.div ``
+export const Container = styled.div `
+
+    height: 100vh;
+
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    background-color: ${props => props.theme.colors.primary};
+
+`
+
+export const Logo = styled.div`
+
+    display: flex;
+    align-items: center;
+
+    margin-bottom: 30px;
+
+    > h3 {
+        color: ${props => props.theme.colors.white};
+        margin-left: 7px;
+    }
+
+    > img {
+        width: 40px;
+        height: 40px;
+    }
+    
+`
+
+export const Form = styled.form`
+
+
+
+`
+
+export const FormTitle = styled.h1`
+
+
+
+
+`
