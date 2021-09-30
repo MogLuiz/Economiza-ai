01925db690f45fe883bd7355728fe6fe11b58878
Luiz Henrique <luizhjramos@outlook.com>
2021-09-30 09:36:12 -0300

    feat: authentication route

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,14 @@
+// Packages
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+// Pages
+import Signin from "../pages/Signin";
+
+const AuthRoutes: React.FC = () => (
+  <Switch>
+    <Route component={Signin} />
+  </Switch>
+);
+
+export default AuthRoutes;
