44410e769e031b5e889c629f3e4499360c7e3e03
Luiz Henrique <luizhjramos@outlook.com>
2021-10-03 15:29:24 -0300

    feat: application responsiveness settings

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -17,7 +17,7 @@ export const Container = styled.div<IContainerProps>`
        
 
     @media(max-width: 600px) {
-        padding-left: 7px;
+        padding-left: 20px;
         position: fixed;
         z-index: 2;
 
